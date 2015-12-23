21205c5f40b2333018fb592374f6da05da4c4e53
bluewaitor <405622394@qq.com>
2015-12-23 22:34:56 +0800

    'module,http.get'

--- learnyounode/05_filteredls.js
+++ learnyounode/05_filteredls.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
+var path = require('path');
 
-fs.readdir(process.argv[2], function  (err, data) {
-	console.log(data);
+fs.readdir(process.argv[2], function(err, list) {
+	list.forEach(function(v){
+		if(path.extname(v) == '.'+process.argv[3]){
+			console.log(v);
+		}
+	})
 });
