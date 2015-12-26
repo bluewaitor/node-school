cffac08f8b00fa6cc8e8682120c3f5e6d6845f45
bluewaitor <405622394@qq.com>
2015-12-26 23:05:52 +0800

    browserify
    学习browserify

---
+++ browserify-adventure/01_beep_boop.js
@@ -0,0 +1,5 @@
+/**
+ * Created by bluewaitor on 15/12/26.
+ */
+
+console.log('BEEP BOOP');
---
+++ browserify-adventure/02_using_npm_packages.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bluewaitor on 15/12/26.
+ */
+
+var uniq = require('uniq');
+var list = prompt('enter a list').split(',');
+console.log(uniq(list));
