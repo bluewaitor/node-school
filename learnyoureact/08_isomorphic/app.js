a16f1d9ea3045fd97372d7f7434ef05ea9153574
bluewaitor <405622394@qq.com>
2015-12-27 15:32:59 +0800

    learnyoureact
    先睡一觉，有空继续

---
+++ learnyoureact/08_isomorphic/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bluewaitor on 15/12/27.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoBox from './views/index.jsx';
+
+let data = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
+ReactDOM.render(<TodoBox data={data}/>, document.getElementById('app'));
+
